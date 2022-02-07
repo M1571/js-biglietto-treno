9b3d844efe34968165d1261169a4f187a4872bfb
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-07 02:09:46 +0100

    age 18

---
+++ main.js
@@ -0,0 +1,13 @@
+var km = parseInt(prompt('Quanti km vuoi percorrere?'));
+console.log(km);
+
+var age = parseInt(prompt('Quanti anni hai?'));
+console.log(age);
+
+// --------------------------------------------
+
+var ticket = (km * 0.21);
+    
+if (age < 18)
+
+// --------------------------------------------
